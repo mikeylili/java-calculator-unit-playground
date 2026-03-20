c5f64a82f241350dd0dfaa0381b8c321f50482d1
mikeylili <a01073111145@gmail.com>
2026-03-20 18:15:05 +0900

    feat: 커스텀 구분자가 주어진 경우

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,11 +1,27 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
-    int split(String expression){
-        String[] tokens = expression.split("[,|:]");
+    int split(String expression) {
+        Pattern pattern = Pattern.compile("//(.*?)\n");
+        Matcher matcher = pattern.matcher(expression);
+        String str = "";
+
+        if (matcher.find()) {
+            str = matcher.group(1);
+            expression = expression.replace("//" + str + "\n", "");
+        }
+
+        String div = ",|:|[" + Pattern.quote(str) + "]";
+        String[] tokens = expression.split(div);
+
         int sum = 0;
         for (String token : tokens) {
-            sum += Integer.parseInt(token);
+            if (!token.isEmpty()) {
+                sum += Integer.parseInt(token.trim());
+            }
         }
         return sum;
     }
