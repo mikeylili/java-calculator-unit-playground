b40017aef56922bf9a0a45cc90c205d72426fb1d
mikeylili <a01073111145@gmail.com>
2026-03-21 16:52:53 +0900

    test: Assert로 리팩터링

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -3,17 +3,18 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
+
     @DisplayName("쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.")
     @Test
-
-    void Test1 (){
+    void Test1() {
         StringCalculator stringCalculator = new StringCalculator();
         int result = stringCalculator.split("5,1:4");
-        assertEquals(10, result);
+
+        assertThat(result).isEqualTo(10);
     }
 
     @DisplayName("커스텀 구분자를 가지는 문자열을 전달하는 경우")
@@ -21,24 +22,25 @@ public class StringCalculatorTest {
     void Test2() {
         StringCalculator stringCalculator = new StringCalculator();
         int result = stringCalculator.split("//;\n3;1;4");
-        assertEquals(8, result);
+
+        assertThat(result).isEqualTo(8);
     }
 
     @DisplayName("음수를 전달하는 경우 RuntimeException 예외를 Throw한다.")
     @Test
-    void Test3() {
+    void testNegativeNumberException() {
         StringCalculator stringCalculator = new StringCalculator();
-        assertThrows(RuntimeException.class, () -> {
-            stringCalculator.split("-1,2,3");
-        });
+
+        assertThatThrownBy(() -> stringCalculator.split("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
     }
 
     @DisplayName("숫자 이외의 값을 전달하는 경우 RuntimeException 예외를 Throw한다.")
     @Test
-    void Test4() {
+    void testNonNumericException() {
         StringCalculator stringCalculator = new StringCalculator();
-        assertThrows(RuntimeException.class, () -> {
-            stringCalculator.split("1,a,3");
-        });
+
+        assertThatThrownBy(() -> stringCalculator.split("1,a,3"))
+                .isInstanceOf(RuntimeException.class);
     }
 }
