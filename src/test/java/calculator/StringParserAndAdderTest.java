763d9ff3737f20c9ed36cc9f9ebabb97dfd8ef5b
mikeylili <a01073111145@gmail.com>
2026-03-23 18:56:30 +0900

    feat: 클래스명 변경, 메소드 기능 분리 및 개행문자 작성

--- src/test/java/calculator/StringParserAndAdderTest.java
+++ src/test/java/calculator/StringParserAndAdderTest.java
@@ -6,13 +6,21 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class StringCalculatorTest {
+public class StringParserAndAdderTest {
 
+    @DisplayName("빈 값을 받았을때 0을 출력한다.")
+    @Test
+    void Test0() {
+        StringParserAndAdder stringParserAndAdder = new StringParserAndAdder();
+        int result = stringParserAndAdder.solver("");
+
+        assertThat(result).isEqualTo(0);
+    }
     @DisplayName("쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.")
     @Test
     void Test1() {
-        StringCalculator stringCalculator = new StringCalculator();
-        int result = stringCalculator.split("5,1:4");
+        StringParserAndAdder stringParserAndAdder = new StringParserAndAdder();
+        int result = stringParserAndAdder.solver("5,1:4");
 
         assertThat(result).isEqualTo(10);
     }
@@ -20,8 +28,8 @@ public class StringCalculatorTest {
     @DisplayName("커스텀 구분자를 가지는 문자열을 전달하는 경우")
     @Test
     void Test2() {
-        StringCalculator stringCalculator = new StringCalculator();
-        int result = stringCalculator.split("//;\n3;1;4");
+        StringParserAndAdder stringParserAndAdder = new StringParserAndAdder();
+        int result = stringParserAndAdder.solver("//;\n3;1;4");
 
         assertThat(result).isEqualTo(8);
     }
@@ -29,18 +37,18 @@ public class StringCalculatorTest {
     @DisplayName("음수를 전달하는 경우 RuntimeException 예외를 Throw한다.")
     @Test
     void testNegativeNumberException() {
-        StringCalculator stringCalculator = new StringCalculator();
+        StringParserAndAdder stringParserAndAdder = new StringParserAndAdder();
 
-        assertThatThrownBy(() -> stringCalculator.split("-1,2,3"))
+        assertThatThrownBy(() -> stringParserAndAdder.solver("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @DisplayName("숫자 이외의 값을 전달하는 경우 RuntimeException 예외를 Throw한다.")
     @Test
     void testNonNumericException() {
-        StringCalculator stringCalculator = new StringCalculator();
+        StringParserAndAdder stringParserAndAdder = new StringParserAndAdder();
 
-        assertThatThrownBy(() -> stringCalculator.split("1,a,3"))
+        assertThatThrownBy(() -> stringParserAndAdder.solver("1,a,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 }
